83204e4cf61356e86c5c823b0f4db1e0df9055d8
DOLLE <jeremy.dolle34@gmail.com>
2017-06-27 17:02:00 +0200

    test images + clearfix resposive

--- js/experiencesBubble.js
+++ js/experiencesBubble.js
@@ -6,26 +6,29 @@
     $.fn.show = function () {
         $(this).each(function () {
             $(this).on('mouseenter', function () {
-                var $target = $(this);
-                var opts = {
-                    title: $target.data('title'),
-                    text: $target.data('text'),
-                    image: $target.data('image')
-                };
-                $target.addClass('hover');
+                var $mousetarget = $(this);
+                if (!$mousetarget.hasClass('hover')){
+                    var opts = {
+                        title: $mousetarget.data('title'),
+                        text: $mousetarget.data('text'),
+                        image: $mousetarget.data('image')
+                    };
+                    $mousetarget.addClass('hover');
 
-                var $brothers = $target  = $(this).siblings();
-                $brothers.each(function () {
-                    $target.removeClass('hover');
-                });
+                    var $brothers = $(this).siblings();
+                    $brothers.each(function () {
+                        $brothers.removeClass('hover');
+                    });
 
-                $target  = $(this).parent().siblings("#experience-text");
-                $target.css('transition','all 0.5s ease 0s');
-                $target.css('transform','translateX(0px)');
-                $target.css('opacity','1');
+                    var $target  = $(this).parent().siblings("#experience-text");
+                    $target.css('transition','all 0.5s ease 0s');
+                    $target.css('transform','translateX(0px)');
+                    $target.css('opacity','1');
 
-                $target.empty();
-                $target.append('<h2>' + opts.title + '</h2><img src="'+ opts.image +'"/><p>' + opts.text + '</p>');
+                    $target.empty();
+                    var html = '<h2>' + opts.title + '</h2><img src="'+ opts.image +'"/><p>' + opts.text + '</p>';
+                    $(html).hide().appendTo($target).fadeIn(700);
+                }
             });
         });
     }
